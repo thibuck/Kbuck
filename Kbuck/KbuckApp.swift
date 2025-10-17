36b11442cdf5a1209af4a71fb54c2b40595cad8e
Augusto Bubick <abubick@kbuckgroup.com>
2025-10-17 14:13:09 -0500

    Initial Commit

---
+++ Kbuck/KbuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  KbuckApp.swift
+//  Kbuck
+//
+//  Created by Augusto Oficina on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct KbuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KbuckTests/KbuckTests.swift
@@ -0,0 +1,16 @@
+//
+//  KbuckTests.swift
+//  KbuckTests
+//
+//  Created by Augusto Oficina on 10/17/25.
+//
+
+import Testing
+
+struct KbuckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
